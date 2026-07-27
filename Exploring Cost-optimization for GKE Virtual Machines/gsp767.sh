1288f093f8bb55f8a34adebc7726a7530eb6a182
Harsh Tiwari <92631120+imharshtiwari@users.noreply.github.com>
2026-07-27 19:10:12 +0530

    Create gsp767.sh

---
+++ Exploring Cost-optimization for GKE Virtual Machines/gsp767.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+TEAL=$'\033[38;5;50m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+BLINK_TEXT=$'\033[5m'
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+REVERSE_TEXT=$'\033[7m'
+
+clear
+
+# Welcome message
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}      SUBSCRIBE SPARKWAVE DEV - INITIATING EXECUTION...  ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Fetching Zone and Region...${RESET_FORMAT}"
+
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Creating Cluster...${RESET_FORMAT}"
+
+gcloud container clusters get-credentials hello-demo-cluster --zone $ZONE
+
+kubectl scale deployment hello-server --replicas=2
+
+gcloud container clusters resize hello-demo-cluster --node-pool my-node-pool \
+    --num-nodes 3 --zone $ZONE --quiet
+
+
+gcloud container node-pools create larger-pool \
+  --cluster=hello-demo-cluster \
+  --machine-type=e2-standard-2 \
+  --num-nodes=1 \
+  --zone=$ZONE
+
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl cordon "$node";
+done
+
+
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=my-node-pool -o=name); do
+  kubectl drain --force --ignore-daemonsets --delete-emptydir-data --grace-period=10 "$node";
+done
+
+
+gcloud container node-pools delete my-node-pool --cluster hello-demo-cluster --zone $ZONE --quiet 
+
+gcloud container clusters create regional-demo --region=$REGION --num-nodes=1
+
+cat << EOF > pod-1.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-1
+  labels:
+    security: demo
+spec:
+  containers:
+  - name: container-1
+    image: wbitt/network-multitool
+EOF
+
+kubectl apply -f pod-1.yaml
+
+cat << EOF > pod-2.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-2
+spec:
+  affinity:
+    podAntiAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+      - labelSelector:
+          matchExpressions:
+          - key: security
+            operator: In
+            values:
+            - demo
+        topologyKey: "kubernetes.io/hostname"
+  containers:
+  - name: container-2
+    image: us-docker.pkg.dev/google-samples/containers/gke/hello-app:1.0
+EOF
+
+kubectl apply -f pod-2.yaml
+
+echo "Waiting for Pod-2 IP..."
+
+while true; do
+  POD2_IP=$(kubectl get pod pod-2 -o jsonpath='{.status.podIP}' 2>/dev/null)
+
+  if [[ -n "$POD2_IP" ]]; then
+    echo "Pod-2 IP acquired: $POD2_IP"
+    break
+  fi
+
+  echo "Still waiting for IP..."
+  sleep 2
+done
+
+
+# Final message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}============================================================================================${RESET_FORMAT_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}       Krlo Subscribe paise nhi lag jayenge (in emotional)  Follow Video For Other Tasks      ${RESET_FORMAT_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}=============================================================================================${RESET_FORMAT_FORMAT}"
+echo
+echo "${RED_TEXT}${BOLD_TEXT}${UNDERLINE_TEXT}https://www.youtube.com/@sparkwavedev${RESET_FORMAT_FORMAT}"
+echo "
